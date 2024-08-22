db641b77b0d48fdecccabc4451b885a39c30d9b9
Oganes <og.marckaryan@yandex.ru>
2024-08-22 12:08:55 +0300

    make work element ... to be continue

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import './style.css'
 
 
 
+const header = document.querySelector('.header-container');
+const main = document.querySelector('.main-container');
+const skills = document.querySelector('.skills');
+// window.addEventListener('scroll', ()=>{
+//     header.classList.add('test')
+// })
