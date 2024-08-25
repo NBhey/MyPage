84499fec5d5d85d79a9c845556e44608aaabb27c
Oganes <og.marckaryan@yandex.ru>
2024-08-25 20:41:11 +0300

    add test and correct block work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import './style.css'
 
 
-
-const header = document.querySelector('.header-container');
-const main = document.querySelector('.main-container');
-const skills = document.querySelector('.skills');
-// window.addEventListener('scroll', ()=>{
-//     header.classList.add('test')
-// })
