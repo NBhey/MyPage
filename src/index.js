cb5698e174f9671c2f9a48613736f98781959539
Oganes <og.marckaryan@yandex.ru>
2024-10-05 15:52:09 +0300

    add toggle night/white

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './style.css'
 
 
+// const body = document.querySelector('body');
+document.getElementById('theme-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
+  });
