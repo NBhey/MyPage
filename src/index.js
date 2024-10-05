130fa78f439e513e37ec114856f2a9bc6c11c098
Oganes <og.marckaryan@yandex.ru>
2024-10-05 21:36:33 +0300

    add toggle night/white step-1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './style.css'
 
 
-// const body = document.querySelector('body');
+
+
+
+if(localStorage.getItem('theme') === 'false') {
+    document.body.classList.add('dark-theme');
+}
 document.getElementById('theme-toggle').addEventListener('click', function () {
+    let darkTheme = localStorage.getItem('theme') == 'false' ? false : true;
+    localStorage.setItem('theme', !darkTheme)
     document.body.classList.toggle('dark-theme');
+   
   });
+
