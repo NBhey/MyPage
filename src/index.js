51487de7b444b4bf72ca591156bbfe99fb4e39a9
Oganes <og.marckaryan@yandex.ru>
2025-12-09 18:16:44 +0300

    добавил тугл переключения темы

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './style.css'
 if(localStorage.getItem('theme') === 'false') {
     document.body.classList.add('dark-theme');
 }
-document.getElementById('theme-toggle').addEventListener('click', function () {
+document.querySelector('.toggle').addEventListener('change', function () {
     let darkTheme = localStorage.getItem('theme') == 'false' ? false : true;
     localStorage.setItem('theme', !darkTheme)
     document.body.classList.toggle('dark-theme');
