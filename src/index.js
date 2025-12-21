ab5d8605e82bb393a7e2d49509c3009bf8695c8d
Oganes <og.marckaryan@yandex.ru>
2025-12-21 21:10:49 +0300

    поменять тугл

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import './style.css'
+import "./style.css";
 
-
-if(localStorage.getItem('theme') === 'false') {
-    document.body.classList.add('dark-theme');
+if (localStorage.getItem("theme") === "false") {
+  document.body.classList.add("dark-theme");
 }
-document.querySelector('.toggle').addEventListener('change', function () {
-    let darkTheme = localStorage.getItem('theme') == 'false' ? false : true;
-    localStorage.setItem('theme', !darkTheme)
-    document.body.classList.toggle('dark-theme');
-   
-  });
+document.querySelector(".toggle").addEventListener("change", function () {
+  let darkTheme = localStorage.getItem("theme") == "false" ? false : true;
+  localStorage.setItem("theme", !darkTheme);
+  document.body.classList.toggle("dark-theme");
+});
 
+const navigationItemsElement = document.querySelector(
+  ".header__navigation-items"
+);
 
-const navigationItemsElement = document.querySelector('.header__navigation-items')
+const burger = document.getElementById("burger");
 
-  const burger = document.getElementById('burger')
-
-  burger.addEventListener('click', ()=>{
-    navigationItemsElement.classList.toggle('active_menu')
-    console.log(123)
-  })
+burger.addEventListener("click", () => {
+  navigationItemsElement.classList.toggle("active_menu");
+  if (navigationItemsElement.classList.contains("active_menu")) {
+    burger.setAttribute("aria-expanded", "true");
+  } else {
+    burger.setAttribute("aria-expanded", "false");
+  }
+  console.log(123);
+});
