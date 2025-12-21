1252a4aac32fe98445e9254c2d00402af8abc9df
Oganes <og.marckaryan@yandex.ru>
2025-12-21 19:01:11 +0300

    добавить выпадающеее мееню

--- src/index.js
+++ src/index.js
@@ -11,3 +11,12 @@ document.querySelector('.toggle').addEventListener('change', function () {
    
   });
 
+
+const navigationItemsElement = document.querySelector('.header__navigation-items')
+
+  const burger = document.getElementById('burger')
+
+  burger.addEventListener('click', ()=>{
+    navigationItemsElement.classList.toggle('active_menu')
+    console.log(123)
+  })
